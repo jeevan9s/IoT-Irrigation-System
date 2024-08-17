2a3cdbe10ece1adc0b01796540c27b5aa95275b5
Jeevan Sanchez <166963895+jeevan9s@users.noreply.github.com>
2024-08-17 00:52:47 -0400

    Add files via upload

---
+++ main.ino
@@ -0,0 +1,131 @@
+/*
+ * Simple example for how to use multiple SinricPro Switch device:
+ * - setup 4 switch devices
+ * - handle request using multiple callbacks
+ * 
+ * If you encounter any issues:
+ * - check the readme.md at https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md
+ * - ensure all dependent libraries are installed
+ *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#arduinoide
+ *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#dependencies
+ * - open serial monitor and check whats happening
+ * - check full user documentation at https://sinricpro.github.io/esp8266-esp32-sdk
+ * - visit https://github.com/sinricpro/esp8266-esp32-sdk/issues and check for existing issues or open a new one
+ */
+
+#define ENABLE_DEBUG
+
+#ifdef ENABLE_DEBUG
+   #define DEBUG_ESP_PORT Serial
+   #define NODEBUG_WEBSOCKETS
+   #define NDEBUG
+#endif 
+
+#include <Arduino.h>
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ESP32) || defined(ARDUINO_ARCH_RP2040)
+  #include <WiFi.h>
+#endif
+
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
+
+#define WIFI_SSID         "Health2020"
+#define WIFI_PASS         "Rexdale2020"
+#define APP_KEY           "a63f77c6-77ec-40cd-9ca3-84de33638e3d"
+#define APP_SECRET        "d4f82be1-7485-4e62-bab2-a9686d6be184-4ed0538e-e6a6-4852-8d77-8884d228f771"
+
+
+#define SWITCH_ID_1       "66b5491b54041e4ff619b5f5"
+#define SWITCH_ID_2       "66b6b2b70d73840bd1a62b41"
+
+#define RELAYPIN_1        13
+#define SENSORPIN_1       12
+
+#define BAUD_RATE         115200                // Change baudrate to your need
+
+#define pumpOPTime        2500
+unsigned long pumpStartTime = 0;
+
+
+bool onPowerState1(const String &deviceId, bool &state) {
+ Serial.printf("Device 1 turned %s", state?"on":"off");
+
+ if (state) {
+  digitalWrite(RELAYPIN_1, LOW);
+  pumpStartTime = millis();
+ }
+
+ else{
+  digitalWrite(RELAYPIN_1, HIGH);
+}
+
+ 
+ return true; // request handled properly
+}
+
+// setup function for WiFi connection
+void setupWiFi() {
+  Serial.printf("\r\n[Wifi]: Connecting");
+
+  #if defined(ESP8266)
+    WiFi.setSleepMode(WIFI_NONE_SLEEP); 
+    WiFi.setAutoReconnect(true);
+  #elif defined(ESP32)
+    WiFi.setSleep(false); 
+    WiFi.setAutoReconnect(true);
+  #endif
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf(".");
+    delay(250);
+  }
+
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+// setup function for SinricPro
+void setupSinricPro() {
+  // add devices and callbacks to SinricPro
+  pinMode(RELAYPIN_1, OUTPUT);
+  pinMode(SENSORPIN_1, INPUT);
+    
+  SinricProSwitch& mySwitch1 = SinricPro[SWITCH_ID_1];
+  mySwitch1.onPowerState(onPowerState1);
+  
+  
+  // setup SinricPro
+  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); }); 
+  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
+  SinricPro.restoreDeviceStates(true); // Uncomment to restore the last known state from the server.
+   
+  SinricPro.begin(APP_KEY, APP_SECRET);
+}
+
+// main setup function
+void setup() {
+  Serial.begin(BAUD_RATE); Serial.printf("\r\n\r\n");
+  setupWiFi();
+  setupSinricPro();
+}
+
+void loop() {
+  SinricPro.handle();
+
+  if (digitalRead(RELAYPIN_1) == LOW) {
+    unsigned long currTime = millis();
+
+    if (currTime - pumpStartTime >= pumpOPTime) {
+      digitalWrite(RELAYPIN_1, HIGH);
+    }
+  }
+
+  int moistureVal = analogRead(SENSORPIN_1);
+  Serial.print("Moisture Level: ");
+  Serial.println(moistureVal);
+
+  
+}
